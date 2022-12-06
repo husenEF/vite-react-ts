883e8e78eea80c4a7441c0412c1229155779e7f5
Husen Efendi <husenef@gmail.com>
2022-12-06 08:27:29 +0700

    add suspend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import router from 'routes/root';
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <RouterProvider router={router} />;
+    </Suspense>
+  );
 }
 
 export default App;
